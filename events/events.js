9e8ed8748ab3be59374d1f98f01585dde21ee677
BuildTools <unconfigured@null.spigotmc.org>
2021-07-10 13:19:07 +0200

    Events revisit

--- events/events.js
+++ events/events.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 module.exports.updateEvents = function(client) {
-    let plugins = fs.readdirSync('./commands');
+    let events = fs.readdirSync('./events');
 
-    for(let i = 0; i < plugins.length; i++) {
-        if(plugins[i] !== 'commands.js' && !commandsList.includes(plugins[i])) {
-            plugins[i].setup(client);
+    for(let i = 0; i < events.length; i++) {
+        if(events[i] !== 'events.js' && !commandsList.includes(events[i])) {
+            events[i].setup(client);
         }
     }
 
